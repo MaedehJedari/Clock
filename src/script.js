af385209e3b3dd036495904bd0c95e9aaeab455a
MaedehJedari <maedeh.jedari@gmail.com>
2024-10-11 18:41:27 +0330

    Added Select-Element

--- src/script.js
+++ src/script.js
@@ -43,3 +43,22 @@ setInterval(function () {
     "h:mm:ss [<small>]A[</small>]"
   )}`;
 }, 1000);
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#whole-cities");
+  citiesElement.innerHTML = `
+  <div class="World-cities">
+        <div>
+          <h2>${cityName}</h2>
+          <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+        </div>
+        <div class="time">${cityTime.format(
+          "h:mm:ss [<small>]A[</small>]"
+        )}</div>
+      </div>`;
+}
+
+let citiesSelect = document.querySelector("#cities");
+citiesSelect.addEventListener("change", updateCity);
